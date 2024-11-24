028c0bfb4adbc4eee41e0cc5762188055b6d0cd3
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-11-24 16:25:23 -0300

    add input component

---
+++ src/Sources/Input.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Reactive.Sources;
+
+public class Input<T> : IFlowable<T, T>
+{
+    event Action<T>? OnFlow;
+
+    public void Emit(T value)
+    {
+        if (OnFlow is null)
+            return;
+        
+        OnFlow(value);
+    }
+
+    public void Subscribe(Action<T> action)
+    {
+        ArgumentNullException.ThrowIfNull(action, nameof(action));
+        OnFlow += action;
+    }
+}
